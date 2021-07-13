b5f3d18421d730b7a0bf2fac0591a300bfc12281
jocobzling <924798429@qq.com>
2021-07-13 16:28:31 +0800

    fix classification bug

--- src/main/java/cn/tp/repositories/PhotoRepository.java
+++ src/main/java/cn/tp/repositories/PhotoRepository.java
@@ -19,7 +19,7 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
     @Query(value = "select * from photo where createTime like %?2% and userId = ?1 order by createTime desc", nativeQuery = true)
     List<Photo> findPhotoByUserIdAndCreateTime(Long userId, String time);
 
-    @Query(value = "select *,max(position) from photo where type in(select mainType from photoType)group by type", nativeQuery = true)
+    @Query(value = "select *,max(position) from photo where userId=?1 and type in(select mainType from photoType)group by type", nativeQuery = true)
     List<Photo> findAllTypeOnePhotoByUserId(Long userId);
 
     @Query(value = "select * from photo where id in(select photoId from FaceClustering where userId = ?2 and clusteringId = ?1)", nativeQuery = true)
